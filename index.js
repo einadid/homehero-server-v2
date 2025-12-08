7c455759b794ca1b58956821d3a87603bc4e5d0b
einadid <emamulislamnadid_cse30d@portcity.edu.bd>
2025-12-08 23:32:34 +0600

    Fixed routing and auth loading issue

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const corsOptions = {
     'http://localhost:5174',
     'http://localhost:3000',
     'https://homehero-client.vercel.app',
+    'https://homehero-client.netlify.app/',
     'https://homehero-client-1rpbzv4kv-einadids-projects.vercel.app',
   ],
   credentials: true,
